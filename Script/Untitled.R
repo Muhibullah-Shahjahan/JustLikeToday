7870b164b549643a9fd829fd86fbac4533e2b36e
muhibullah-shahjahan <muhibullah018@gmail.com>
2026-06-14 23:40:26 +0600

    Initial commit

--- Script/Untitled.R
+++ Script/Untitled.R
@@ -13,3 +13,12 @@ library(tidyverse)
 library(tidyverse)
 
 library(tidyverse)
+
+
+library('tidyverse')
+install.packages("usethis") 
+library(usethis) 
+use_git_config(user.name = "muhibullah-shahjahan", user.email = "muhibullah018@gmail.com") 
+# Personal access token for HTTPS create_github_token() # Or, go to https://github.com/settings/tokens and click “Generate toke"
+
+gitcreds::gitcreds_set()
